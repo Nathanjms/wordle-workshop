5c6caceb93182afc1682b92072fd4f7d4dde6e6b
Nathan James <nathanjms@outlook.com>
2022-12-02 15:11:26 +0000

    Vanilla javascript grid

--- public/app.js
+++ public/app.js
@@ -1,1 +1,19 @@
-console.log("hello");
+let grid = document.querySelector("#game");
+
+let fragment = document.createDocumentFragment();
+
+const wordCount = 3;
+const attempts = 3;
+
+Array.from({ length: attempts }).forEach(() => {
+  let row = document.createElement("div");
+  row.classList.add("row");
+  Array.from({ length: wordCount }).forEach(() => {
+    let tile = document.createElement("div");
+    tile.classList.add("tile");
+    row.appendChild(tile);
+  });
+  fragment.appendChild(row);
+});
+
+grid.appendChild(fragment);
