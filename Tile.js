212cfbdae63cccdf3abf1b1f08973202ebf6febf
Nathan James <nathanjms@outlook.com>
2022-12-16 12:11:43 +0000

    Add todo

--- Tile.js
+++ Tile.js
@@ -11,6 +11,7 @@ export default class Tile {
   }
 
   updateStatus(theWord, index) {
+    // TODO: handle same letter being input twice saying its valid!!
     if (!theWord.includes(this.letter)) {
       this.status = "absent";
     } else if (this.letter === theWord[index]) {
